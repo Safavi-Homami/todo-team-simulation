6794684015d75ed32fa7ef5601fca039962ec43d
Safavi-Homami <safawiden@gmx.net>
2025-03-26 11:52:01 +0100

    TodoService hinzugefügt und Main angepasst

---
+++ src/main/java/com/safavi/todo/Main.java
@@ -0,0 +1,11 @@
+package com.safavi.todo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TodoService service = new TodoService();
+        service.addTodo("Pull Request üben");
+        service.addTodo("Konflikte lösen");
+        service.printTodos();
+    }
+}
---
+++ src/main/java/com/safavi/todo/TodoService.java
@@ -0,0 +1,19 @@
+package com.safavi.todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoService {
+    private final List<String> todos = new ArrayList<>();
+
+    public void addTodo(String todo) {
+        todos.add(todo);
+    }
+
+    public void printTodos() {
+        System.out.println("Aktuelle Todos:");
+        for (String todo : todos) {
+            System.out.println("- " + todo);
+        }
+    }
+}
